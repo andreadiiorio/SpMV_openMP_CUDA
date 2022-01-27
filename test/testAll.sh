d0dfe3fec6238ab14a4ca966b3083dc80aeb625b
Andrea Di Iorio <andrea.diiorio.sniper@gmail.com>
2022-01-27 17:23:27 +0100

    testAll generalized with

--- test/testAll.sh
+++ test/testAll.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-set -e -o pipefail
-
-MATRIX_DIR=~/data/prj/main/
-MATRIX_LIST=$1	#log/testAllCUDA_errors
+#WRAP TEST SCRIPT. EXPORT  OMP or CUDA for different set of tests
+#export DEBUG for debug calls, and the target matrix to use in $1
 
 mkdir -p log
-#CUDA
-#TEST_BIN=./test_SpMV_CUDA_Stats.o
-TEST_BIN=./test_SpMV_CUDA_DBG.o
 
+MATRIX_DIR=~/data/prj/main/
+matrixes=( $(find $MATRIX_DIR  -name "*mtx" | shuf) )
+matrixesList=( $(cat "$1" | shuf ) )	#matrixes given in input file list
+trgtMatrixes=${matrixes[@]}
+
+set -e -o pipefail
 if [ $CUDA ];then
-	#for m in $( find $MATRIX_DIR  -name "*mtx" | shuf);do 
-	for m in $( cat $MATRIX_LIST | shuf );do
+	TEST_BIN=./test_SpMV_CUDA_Stats.o
+	if [ $DEBUG ];then TEST_BIN=./test_SpMV_CUDA_DBG.o;	trgtMatrixes=${matrixesList[@]};fi
+	for m in ${matrixes[@]};do
 	    echo "#$m" ; ( $TEST_BIN $m RNDVECT | tee -a "log/testAllCUDA" ) || echo $m | tee -a log/testAllCUDA_errors
 	done; echo echo "done CUDA";
 elif [ $OMP ];then
-	#OMP
-	TEST_BIN=./test_SpMV_OMP_DBG.o;
-	#TEST_BIN=./test_SpMV_OMP_Stats;
+	TEST_BIN=./test_SpMV_OMP_Stats.o;
+	if [ $DEBUG ];then TEST_BIN=./test_SpMV_OMP_DBG.o;	trgtMatrixes=${matrixesList[@]};fi
 	export GRID_ROWS=8 GRID_COLS=2
-	#for m in $( find $MATRIX_DIR  -name "*mtx.gz" | shuf);do
-	for m in $( cat $MATRIX_LIST | shuf );do
+	for m in ${matrixes[@]};do
 		echo "#$m" ; ( $TEST_BIN $m RNDVECT | tee "log/testAll"$GRID_ROWS"X"$GRID_COLS"_"$OMP_SCHEDULE ) || echo $m | tee -a log/testAllOMP_errors  
 	done; echo echo "done $GRID_ROWS X $GRID_COLS"
 else echo "EXPORT EITHER CUDA or OMP"
